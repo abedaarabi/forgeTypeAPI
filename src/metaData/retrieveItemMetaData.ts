fe2b6766cd00bb77d53ae1515bf628bec86c3e12
Abdulrahman Mohamad Aarabi <abma@moe.dk>
2022-02-07 14:50:57 +0100

    topLevelObj

--- src/metaData/retrieveItemMetaData.ts
+++ src/metaData/retrieveItemMetaData.ts
@@ -15,6 +15,43 @@ import { hub } from 'src/shared/forge.hub';
 import { Logger } from '@nestjs/common';
 import { publishCloudWorkshared } from 'src/publishModel/publishCloudWorkshared';
 
+const dummyObject = {
+  name: 'key not exist',
+  dbId: 'key not exist',
+  version_id: 'key not exist',
+  externalId: 'key not exist',
+  TypeName: 'key not exist',
+  objectId: 'key not exist',
+  Workset: 'key not exist',
+  Type_Sorting: 'key not exist',
+  CCSTypeID: 'key not exist',
+  CCSTypeID_Type: 'key not exist',
+  CCSClassCode_Type: 'key not exist',
+  BIM7AATypeName: 'key not exist',
+  BIM7AATypeDescription: 'key not exist',
+  BIM7AATypeID: 'key not exist',
+  BIM7AATypeNumber: 'key not exist',
+  BIM7AATypeCode: 'key not exist',
+  BIM7AATypeComments: 'key not exist',
+};
+const isObj = (obj) => typeof obj === 'object' && !Array.isArray(obj);
+const topLevelObj = (obj) => {
+  let item = {};
+  for (const key in obj) {
+    if (isObj(obj[key])) {
+      topLevelObj(obj[key]);
+
+      item = { ...item, ...topLevelObj(obj[key]) };
+    } else {
+      if (Object.keys(dummyObject).includes(key)) {
+        // item[key] = obj[key];
+        item = { ...item, [key]: obj[key] };
+      }
+    }
+  }
+  return item;
+};
+
 export type ElementProperties = {
   name: string;
   dbId: number;
@@ -44,11 +81,10 @@ export const propertiesMetadata = async () => {
 
   const hasGuId = allItemsMetaData.filter((item) => {
     if (
-      item.guid &&
-      (item.fileName.includes('K07') ||
-        item.fileName.includes('K08') ||
-        item.fileName.includes('K09') ||
-        item.fileName.includes('K10'))
+      (item.guid && item.fileName.includes('K07')) ||
+      item.fileName.includes('K08') ||
+      item.fileName.includes('K09') ||
+      item.fileName.includes('K10')
     ) {
       return true;
     } else return false;
@@ -61,6 +97,8 @@ export const propertiesMetadata = async () => {
 
     while (true) {
       try {
+        console.log(itemMetaData.guid);
+
         const itemProperties = await guid.getModelviewProperties(
           itemMetaData.derivativesId,
           itemMetaData.guid,
@@ -110,33 +148,22 @@ export const propertiesMetadata = async () => {
     const elementsCount = property.hasTypeName.length as string;
     const date = property.itemMetaData.lastModifiedTime as string;
     const modiId = id.split('?')[0] as string;
-    const version_id = id.split('=')[1];
+    const version_id = Number(id.split('=')[1]);
     Logger.debug(version_id);
 
     const eltCollection = property.hasTypeName.map((elt) => {
-      const dbId = elt.name.split('[')[1].split(']')[0];
+      const dbId = Number(elt.name.split('[')[1].split(']')[0]);
 
       // return elt;
-      return {
-        name: elt.name,
-        dbId: Number(dbId),
-        version_id: Number(version_id),
-        externalId: elt['externalId'],
-        TypeName: elt.properties['Identity Data']['Type Name'],
-        objectId: property.itemMetaData.versionId,
-        Workset: elt.properties['Identity Data']['Workset'],
-        Type_Sorting: elt.properties['Identity Data']['Type Sorting'],
-        CCSTypeID: elt.properties['Other']['CCSTypeID'],
-        CCSTypeID_Type: elt.properties['Other']['CCSTypeID_Type'],
-        CCSClassCode_Type: elt.properties['Other']['CCSClassCode_Type'],
-        BIM7AATypeName: elt.properties['Other']['BIM7AATypeName'],
-        BIM7AATypeDescription: elt.properties['Other']['BIM7AATypeDescription'],
-        BIM7AATypeID: elt.properties['Other']['BIM7AATypeID'],
-        BIM7AATypeNumber: elt.properties['Other']['BIM7AATypeNumber'],
-        BIM7AATypeCode: elt.properties['Other']['BIM7AATypeCode'],
-        BIM7AATypeComments: elt.properties['Other']['BIM7AATypeComments'],
-      };
-    }) as ElementProperties[];
+      const objResult = {
+        ...dummyObject,
+        dbId,
+        version_id,
+        ...topLevelObj(elt),
+      } as ElementProperties;
+
+      return objResult;
+    });
 
     //calling the database
     await deleteObjId(modiId);
@@ -150,7 +177,7 @@ export const propertiesMetadata = async () => {
     });
 
     await insterElements(eltCollection);
-    Logger.debug('*****************DONE*****************');
+    Logger.debug('*****************DONE*****************' + name + projectName);
 
     await delay(10 * 1000);
   }
@@ -159,4 +186,23 @@ export const propertiesMetadata = async () => {
   return '*****************DONE*****************';
 };
 
-/**/
+// return;
+// const returnObj = {
+//   name: elt.name,
+//   dbId: Number(dbId),
+//   version_id: Number(version_id),
+//   externalId: elt['externalId'],
+//   TypeName: elt.properties['Identity Data']['Type Name'],
+//   objectId: property.itemMetaData.versionId,
+//   Workset: elt.properties['Identity Data']['Workset'],
+//   Type_Sorting: elt.properties['Identity Data']['Type Sorting'],
+//   CCSTypeID: elt.properties['Other']['CCSTypeID'],
+//   CCSTypeID_Type: elt.properties['Other']['CCSTypeID_Type'],
+//   CCSClassCode_Type: elt.properties['Other']['CCSClassCode_Type'],
+//   BIM7AATypeName: elt.properties['Other']['BIM7AATypeName'],
+//   BIM7AATypeDescription: elt.properties['Other']['BIM7AATypeDescription'],
+//   BIM7AATypeID: elt.properties['Other']['BIM7AATypeID'],
+//   BIM7AATypeNumber: elt.properties['Other']['BIM7AATypeNumber'],
+//   BIM7AATypeCode: elt.properties['Other']['BIM7AATypeCode'],
+//   BIM7AATypeComments: elt.properties['Other']['BIM7AATypeComments'],
+// };
--- src/shared/forge.projects.ts
+++ src/shared/forge.projects.ts
@@ -33,13 +33,11 @@ export const projects = async (hubId) => {
   const neededProjects = [
     // 'b.12469613-6f90-42c3-b49c-4bb22e440e30',
     // 'b.9789bc25-07fa-4fdc-9415-9951e8865d9d', //B313
-
     // 'b.a9a41e9c-3a75-4fca-bb88-acd926c7bbdc',
     // 'b.971c280a-ba24-4366-8b3c-25ef5cd82511',
-
     // 'b.c74e65d3-cb3f-4863-9378-479ce0d4f995', //NOL
     // 'b.0063f0d4-abdc-4d09-851e-3aa550c8dc81',
-    // 'b.5449d573-5229-45c1-832c-371769347b35',
+
     // 'b.5172f7a8-7376-4765-a871-ba3b8fcce946',
     // 'b.b85d407c-f4ed-41d1-b42a-2796292dbf0b',
     'b.40365bf1-68ff-4782-91ed-9e8c404aa9c2',
